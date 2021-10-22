e60458408afc262d8c840680cc1b62ce946d4b54
Ivan Caviedes <ivancaviedes99@gmail.com>
2021-10-22 15:58:09 -0500

    precios y footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,32 @@
     return false;
   });
 
+
+  $(window).on('load', function() {
+
+    var portfolioIsotope = $('.pricing-container').isotope({
+      itemSelector: '.pricing-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#pricing-flters li').on('click', function() {
+      $("#pricing-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
+
+
+
+
   $(window).on('load', function() {
 
     var portfolioIsotope = $('.portfolio-container').isotope({
