bb13e026485ee5201b1cb43dc3de687caf8771b4
Ivan Caviedes <ivancaviedes99@gmail.com>
2021-12-08 19:44:44 -0500

    arrelando datas

--- assets/js/datos.js
+++ assets/js/datos.js
@@ -1,7 +1,11 @@
+let baseImagenes = "assets/img/"
 
+
+// Preguntas Frecuentes
 let t = document.querySelector('#preguntas').content
 const lista = document.querySelector("#listapreguntas")
 const fragment = document.createDocumentFragment();
+
 const preguntasFrecuentes = [
     {
         id: "1",
@@ -12,8 +16,14 @@ const preguntasFrecuentes = [
         id: "2",
         pregunta: "Pregunta 2",
         solucion: "Solucion 2"
+    },
+    {
+        id: "2",
+        pregunta: "esta es una pregunta",
+        solucion: "esta es una solucion Solucion"
     }
 ]
+
 preguntasFrecuentes.forEach(item => {
 
     t.querySelector(".col-lg-5 h4").textContent = item.pregunta
@@ -25,6 +35,7 @@ preguntasFrecuentes.forEach(item => {
 })
 lista.appendChild(fragment)
 
+// Testimonios
 
 let t1 = document.querySelector('#testimonios').content
 const lista1 = document.querySelector("#listatestimonial")
@@ -53,9 +64,9 @@ Testimonios.forEach(item => {
     const clone = t1.cloneNode(true)
     fragment1.appendChild(clone)
 })
-
 lista1.appendChild(fragment1)
 
+// Nuestro Equipo
 
 let t2 = document.querySelector('#teamember').content
 const lista2 = document.querySelector("#listeam")
@@ -64,18 +75,36 @@ const fragment2 = document.createDocumentFragment();
 const Team = [
     {
         id: "1",
-        nombre: "ivan Caviedes",
-        cargo: "Admin",
-    },
-    {
-        id: "2",
-        nombre: "Carlos andres",
-        cargo: "trabajador",
+        nombre: "Ivan",
+        cargo: "seo",
+        redes: {
+            facebook: "https://www.facebook.com/Ivancaviedes09/",
+            twitter: "",
+            instagram: "",
+            linkedin: ""
+        }
     },
     {
         id: "2",
         nombre: "Daniela",
-        cargo: "Diseñadora",
+        cargo: "Diseñadora Grafica",
+        redes: {
+            facebook: "",
+            twitter: "",
+            instagram: "",
+            linkedin: ""
+        }
+    },
+    {
+        id: "3",
+        nombre: "Carlos",
+        cargo: "Ingeniero",
+        redes: {
+            facebook: "",
+            twitter: "",
+            instagram: "",
+            linkedin: ""
+        }
     }
 ]
 
@@ -83,10 +112,97 @@ const Team = [
 Team.forEach(item => {
     t2.querySelector(".member h4").textContent = item.nombre
     t2.querySelector(".member span").textContent = item.cargo
-
+    t2.querySelector(".member .pic img").src = `${baseImagenes}/team/team-${item.id}.jpg`
+    t2.querySelector(".member .social #facebook").href = item.redes.facebook
+    t2.querySelector(".member .social #twitter").href = item.redes.twitter
+    t2.querySelector(".member .social #instagram").href = item.redes.instagram
+    t2.querySelector(".member .social #linkedin").href = item.redes.linkedin
     const clone = t2.cloneNode(true)
     fragment2.appendChild(clone)
 })
 
 lista2.appendChild(fragment2)
 
+
+// Nuestros Trabajos
+
+
+let t3 = document.querySelector('#trabajos').content
+const lista3 = document.querySelector("#listrabajos")
+const fragment3 = document.createDocumentFragment();
+
+const Trabajos = [
+    {
+        id: "1",
+        imagen: "",
+        link: "",
+        titulo: "",
+        descripcion: "",
+        clase: "filter-web"
+
+    },
+    {
+        id: "1",
+        imagen: "",
+        link: "",
+        titulo: "",
+        descripcion: "",
+        clase: "filter-Ilustraciones"
+
+    }
+]
+
+Trabajos.forEach(item => {
+    t3.querySelector("#item-trabajo").classList.add(item.clase);
+    const clone = t3.cloneNode(true)
+    fragment3.appendChild(clone)
+})
+
+lista3.appendChild(fragment3)
+
+// Nuestros servicios
+let t4 = document.querySelector('#servicios').content
+const lista4 = document.querySelector("#listaservicios")
+const fragment4 = document.createDocumentFragment();
+
+let Servicios = [
+    {
+        imagen:"bannerAppsWeb.jpg",
+        titulo:"Aplicaciones Web",
+        descripcion:"descripcion"
+    },
+    {
+        imagen:"bannerAppsPersonalizada.jpg",
+        titulo:"Aplicaciones Personalizadas",
+        descripcion:"descripcion"
+    },
+    {
+        imagen:"bannerAppsMovil.jpg",
+        titulo:"Aplicaciones Movil",
+        descripcion:"descripcion"
+    },
+    {
+        imagen:"ux.jpg",
+        titulo:"Diseño UX",
+        descripcion:"descripcion"
+    },
+    {
+        imagen:"ui.png",
+        titulo:"Diseño UI",
+        descripcion:"descripcion"
+    },
+    {
+        imagen:"photografia.jpg",
+        titulo:"Fotografia",
+        descripcion:"descripcion"
+    }
+]
+Servicios.forEach(item => {
+    t4.querySelector(".card-img img").src = `${baseImagenes}${item.imagen}`
+    t4.querySelector(".card-title").textContent = item.titulo
+    t4.querySelector(".card-text").textContent = item.descripcion
+    const clone = t4.cloneNode(true)
+    fragment4.appendChild(clone)
+})
+
+lista4.appendChild(fragment4)
