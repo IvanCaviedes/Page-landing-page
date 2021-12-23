f73cb9660a4c5e5a948ef3ee7328a0c1e0c9be66
jhhg0 <jhherrera@ccss.sa.cr>
2021-12-23 18:29:49 -0500

    add team member

--- assets/js/datos.js
+++ assets/js/datos.js
@@ -1,208 +1,219 @@
-let baseImagenes = "assets/img/"
-
+let baseImagenes = 'assets/img/';
 
 // Preguntas Frecuentes
-let t = document.querySelector('#preguntas').content
-const lista = document.querySelector("#listapreguntas")
+let t = document.querySelector('#preguntas').content;
+const lista = document.querySelector('#listapreguntas');
 const fragment = document.createDocumentFragment();
 
 const preguntasFrecuentes = [
-    {
-        id: "1",
-        pregunta: "Pregunta 1",
-        solucion: "Solucion 1"
-    },
-    {
-        id: "2",
-        pregunta: "Pregunta 2",
-        solucion: "Solucion 2"
-    },
-    {
-        id: "2",
-        pregunta: "esta es una pregunta",
-        solucion: "esta es una solucion Solucion"
-    }
-]
+  {
+    id: '1',
+    pregunta: 'Pregunta 1',
+    solucion: 'Solucion 1',
+  },
+  {
+    id: '2',
+    pregunta: 'Pregunta 2',
+    solucion: 'Solucion 2',
+  },
+  {
+    id: '2',
+    pregunta: 'esta es una pregunta',
+    solucion: 'esta es una solucion Solucion',
+  },
+];
 
-preguntasFrecuentes.forEach(item => {
+preguntasFrecuentes.forEach((item) => {
+  t.querySelector('.col-lg-5 h4').textContent = item.pregunta;
+  t.querySelector('.col-lg-7 p').textContent = item.solucion;
+  const clone = t.cloneNode(true);
 
-    t.querySelector(".col-lg-5 h4").textContent = item.pregunta
-    t.querySelector(".col-lg-7 p").textContent = item.solucion
-    const clone = t.cloneNode(true)
-
-    fragment.appendChild(clone)
-
-})
-lista.appendChild(fragment)
+  fragment.appendChild(clone);
+});
+lista.appendChild(fragment);
 
 // Testimonios
 
-let t1 = document.querySelector('#testimonios').content
-const lista1 = document.querySelector("#listatestimonial")
+let t1 = document.querySelector('#testimonios').content;
+const lista1 = document.querySelector('#listatestimonial');
 const fragment1 = document.createDocumentFragment();
 
 const Testimonios = [
-    {
-        id: "1",
-        text: "Este es un testimonio cargado por medio de js",
-        author: "autor 1",
-        occupacion: "el loco"
-    },
-    {
-        id: "2",
-        text: "etxto2",
-        author: "autor 2",
-        occupacion: "ninguna"
-    }
-]
+  {
+    id: '1',
+    text: 'Este es un testimonio cargado por medio de js',
+    author: 'autor 1',
+    occupacion: 'el loco',
+  },
+  {
+    id: '2',
+    text: 'etxto2',
+    author: 'autor 2',
+    occupacion: 'ninguna',
+  },
+];
 
-Testimonios.forEach(item => {
-    t1.querySelector(".testimonial-text").textContent = item.text
-    t1.querySelector(".testimonial-author").textContent = item.author
-    t1.querySelector(".occupation").textContent = item.occupacion
+Testimonios.forEach((item) => {
+  t1.querySelector('.testimonial-text').textContent = item.text;
+  t1.querySelector('.testimonial-author').textContent = item.author;
+  t1.querySelector('.occupation').textContent = item.occupacion;
 
-    const clone = t1.cloneNode(true)
-    fragment1.appendChild(clone)
-})
-lista1.appendChild(fragment1)
+  const clone = t1.cloneNode(true);
+  fragment1.appendChild(clone);
+});
+lista1.appendChild(fragment1);
 
 // Nuestro Equipo
 
-let t2 = document.querySelector('#teamember').content
-const lista2 = document.querySelector("#listeam")
+let t2 = document.querySelector('#teamember').content;
+const lista2 = document.querySelector('#listeam');
 const fragment2 = document.createDocumentFragment();
 
 const Team = [
-    {
-        id: "1",
-        nombre: "Iván Caviedes",
-        cargo: "seo",
-        redes: {
-            facebook: "https://www.facebook.com/Ivancaviedes09/",
-            twitter: "",
-            instagram: "",
-            linkedin: ""
-        }
+  {
+    id: '1',
+    nombre: 'Iván Caviedes',
+    cargo: 'seo',
+    redes: {
+      facebook: 'https://www.facebook.com/Ivancaviedes09/',
+      twitter: '',
+      instagram: '',
+      linkedin: '',
     },
-    {
-        id: "2",
-        nombre: "Daniela Fresneda",
-        cargo: "Diseñadora Grafica",
-        redes: {
-            facebook: "",
-            twitter: "",
-            instagram: "",
-            linkedin: ""
-        }
+  },
+  {
+    id: '2',
+    nombre: 'Daniela Fresneda',
+    cargo: 'Diseñadora Grafica',
+    redes: {
+      facebook: '',
+      twitter: '',
+      instagram: '',
+      linkedin: '',
     },
-    {
-        id: "3",
-        nombre: "Andres Castillo",
-        cargo: "Ingeniero",
-        redes: {
-            facebook: "",
-            twitter: "",
-            instagram: "",
-            linkedin: ""
-        }
-    }
-]
+  },
+  {
+    id: '3',
+    nombre: 'Andres Castillo',
+    cargo: 'Ingeniero',
+    redes: {
+      facebook: '',
+      twitter: '',
+      instagram: '',
+      linkedin: '',
+    },
+  },
+  {
+    id: '3',
+    nombre: 'John Herrera',
+    cargo: 'Developer',
+    redes: {
+      facebook: '',
+      twitter: '',
+      instagram: '',
+      linkedin: '',
+    },
+  },
+];
 
+Team.forEach((item) => {
+  t2.querySelector('.member h4').textContent = item.nombre;
+  // t2.querySelector(".member span").textContent = item.cargo
+  t2.querySelector(
+    '.member .pic img'
+  ).src = `${baseImagenes}/team/team-${item.id}.jpg`;
+  t2.querySelector('.member .social #facebook').href = item.redes.facebook;
+  // t2.querySelector(".member .social #twitter").href = item.redes.twitter
+  t2.querySelector('.member .social #instagram').href = item.redes.instagram;
+  t2.querySelector('.member .social #linkedin').href = item.redes.linkedin;
+  const clone = t2.cloneNode(true);
+  fragment2.appendChild(clone);
+});
 
-Team.forEach(item => {
-    t2.querySelector(".member h4").textContent = item.nombre
-    // t2.querySelector(".member span").textContent = item.cargo
-    t2.querySelector(".member .pic img").src = `${baseImagenes}/team/team-${item.id}.jpg`
-    t2.querySelector(".member .social #facebook").href = item.redes.facebook
-    // t2.querySelector(".member .social #twitter").href = item.redes.twitter
-    t2.querySelector(".member .social #instagram").href = item.redes.instagram
-    t2.querySelector(".member .social #linkedin").href = item.redes.linkedin
-    const clone = t2.cloneNode(true)
-    fragment2.appendChild(clone)
-})
-
-lista2.appendChild(fragment2)
-
+lista2.appendChild(fragment2);
 
 // Nuestros Trabajos
 
-
-let t3 = document.querySelector('#trabajos').content
-const lista3 = document.querySelector("#listrabajos")
+let t3 = document.querySelector('#trabajos').content;
+const lista3 = document.querySelector('#listrabajos');
 const fragment3 = document.createDocumentFragment();
 
 const Trabajos = [
-    {
-        id: "1",
-        imagen: "",
-        link: "",
-        titulo: "",
-        descripcion: "",
-        clase: "filter-web"
+  {
+    id: '1',
+    imagen: '',
+    link: '',
+    titulo: '',
+    descripcion: '',
+    clase: 'filter-web',
+  },
+  {
+    id: '1',
+    imagen: '',
+    link: '',
+    titulo: '',
+    descripcion: '',
+    clase: 'filter-Ilustraciones',
+  },
+];
 
-    },
-    {
-        id: "1",
-        imagen: "",
-        link: "",
-        titulo: "",
-        descripcion: "",
-        clase: "filter-Ilustraciones"
+Trabajos.forEach((item) => {
+  t3.querySelector('#item-trabajo').classList.add(item.clase);
+  const clone = t3.cloneNode(true);
+  fragment3.appendChild(clone);
+});
 
-    }
-]
-
-Trabajos.forEach(item => {
-    t3.querySelector("#item-trabajo").classList.add(item.clase);
-    const clone = t3.cloneNode(true)
-    fragment3.appendChild(clone)
-})
-
-lista3.appendChild(fragment3)
+lista3.appendChild(fragment3);
 
 // Nuestros servicios
-let t4 = document.querySelector('#servicios').content
-const lista4 = document.querySelector("#listaservicios")
+let t4 = document.querySelector('#servicios').content;
+const lista4 = document.querySelector('#listaservicios');
 const fragment4 = document.createDocumentFragment();
 
 let Servicios = [
-    {
-        imagen:"bannerAppsWeb.jpg",
-        titulo:"Aplicaciones Web",
-        descripcion:"Somos una empresa enfocada al desarrollo a la medida, creamos sitios web y diseño web. ofrecemos la una solucion completa mejorar tu presencia en internet."
-    },
-    {
-        imagen:"bannerAppsPersonalizada.jpg",
-        titulo:"Tiendas Online",
-        descripcion:"Aumenta las ventas de tu negocio con una tienda online. Son realizadas por profecionales enfocados a el aumento de produccion."
-    },
-    {
-        imagen:"bannerAppsMovil.jpg",
-        titulo:"Aplicaciones Movil",
-        descripcion:"Diseñamos aplicaciones para moviles (Android o ios) ajustandonos a tus necesidades y ofreciendo la una solucion completa mejorar tu presencia las tiendas mobiles."
-    },
-    {
-        imagen:"ux.jpg",
-        titulo:"Diseño UX",
-        descripcion:"Diseñamos aplicaciones utilizando un enfoque de diseño resposive. Somos desarrolladores que creamos interfaces y estrategias web."
-    },
-    {
-        imagen:"ui.png",
-        titulo:"Diseño UI",
-        descripcion:"Diseñamos mockups de tus aplicaciones utilizando un enfoque de diseño resposive. Creamos un interfaces de usuario que te ayudara a guiar a tus clientes por las diferentes partes de tu aplicacion."
-    },
-    {
-        imagen:"photografia.jpg",
-        titulo:"Fotografia",
-        descripcion:"Atrae clientes con fotografias profecionales, Nuestros fotografos tienen en cuenta la calidad la prespectiva y el angulo ideal para que tengas un exelente resultado en lo esperado."
-    }
-]
-Servicios.forEach(item => {
-    t4.querySelector(".card-img img").src = `${baseImagenes}${item.imagen}`
-    t4.querySelector(".card-title").textContent = item.titulo
-    t4.querySelector(".card-text").textContent = item.descripcion
-    const clone = t4.cloneNode(true)
-    fragment4.appendChild(clone)
-})
+  {
+    imagen: 'bannerAppsWeb.jpg',
+    titulo: 'Aplicaciones Web',
+    descripcion:
+      'Somos una empresa enfocada al desarrollo a la medida, creamos sitios web y diseño web. ofrecemos la una solucion completa mejorar tu presencia en internet.',
+  },
+  {
+    imagen: 'bannerAppsPersonalizada.jpg',
+    titulo: 'Tiendas Online',
+    descripcion:
+      'Aumenta las ventas de tu negocio con una tienda online. Son realizadas por profecionales enfocados a el aumento de produccion.',
+  },
+  {
+    imagen: 'bannerAppsMovil.jpg',
+    titulo: 'Aplicaciones Movil',
+    descripcion:
+      'Diseñamos aplicaciones para moviles (Android o ios) ajustandonos a tus necesidades y ofreciendo la una solucion completa mejorar tu presencia las tiendas mobiles.',
+  },
+  {
+    imagen: 'ux.jpg',
+    titulo: 'Diseño UX',
+    descripcion:
+      'Diseñamos aplicaciones utilizando un enfoque de diseño resposive. Somos desarrolladores que creamos interfaces y estrategias web.',
+  },
+  {
+    imagen: 'ui.png',
+    titulo: 'Diseño UI',
+    descripcion:
+      'Diseñamos mockups de tus aplicaciones utilizando un enfoque de diseño resposive. Creamos un interfaces de usuario que te ayudara a guiar a tus clientes por las diferentes partes de tu aplicacion.',
+  },
+  {
+    imagen: 'photografia.jpg',
+    titulo: 'Fotografia',
+    descripcion:
+      'Atrae clientes con fotografias profecionales, Nuestros fotografos tienen en cuenta la calidad la prespectiva y el angulo ideal para que tengas un exelente resultado en lo esperado.',
+  },
+];
+Servicios.forEach((item) => {
+  t4.querySelector('.card-img img').src = `${baseImagenes}${item.imagen}`;
+  t4.querySelector('.card-title').textContent = item.titulo;
+  t4.querySelector('.card-text').textContent = item.descripcion;
+  const clone = t4.cloneNode(true);
+  fragment4.appendChild(clone);
+});
 
-lista4.appendChild(fragment4)
+lista4.appendChild(fragment4);
